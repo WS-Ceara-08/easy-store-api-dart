a649f345290080706faa368c4a0a2746b4b7cfa2
Gab.Braga <gabriel.lima21031@gmail.com>
2024-10-29 14:54:11 -0300

    Servidor concluido
    Funções de conexão e fechamento feitas
    
    Configurações feitas puxando do .env

--- lib/DB/server.dart
+++ lib/DB/server.dart
@@ -2,7 +2,36 @@ import 'package:mysql1/mysql1.dart';
 
 class Server {
   ConnectionSettings settings = ConnectionSettings(
-    host: String.fromEnvironment('DbHost'),
-    //etc....
-  );
+      host: String.fromEnvironment('DbHost'),
+      port: int.fromEnvironment('DbPort'),
+      user: String.fromEnvironment('DbUser'),
+      password: String.fromEnvironment('DbPass'),
+      db: String.fromEnvironment('Db'));
+
+  MySqlConnection? connection;
+
+  Future<MySqlConnection?> connect() async {
+    try {
+      connection ??= await MySqlConnection.connect(settings);
+      print('Conexão concluida com sucesso');
+    } catch (e) {
+      connection = null;
+      print('Conexão falhou');
+    } finally {
+      // ignore: control_flow_in_finally
+      return connection;
+    }
+  }
+
+  Future<void> close() async {
+    try {
+      if (connection != null) {
+        await connection!.close();
+        connection = null;
+      }
+      print('Conexão fechada com sucesso');
+    } catch (e) {
+      print('Conexão não conseguiu fechar');
+    }
+  }
 }
